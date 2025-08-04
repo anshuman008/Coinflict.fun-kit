de398a41569cc6e7c9e5b53c4fe522a1c8c887c2
anshuman008 <code.anshu@gmail.com>
2025-08-05 05:21:47 +0530

    chore: remove test files from repository

--- test-package.js
+++
@@ -1,6 +0,0 @@
-// Simple test to verify the package works
-const { CoinFlictSdk } = require('./dist/index.js');
-
-console.log('✅ Package loaded successfully!');
-console.log('CoinFlictSdk:', typeof CoinFlictSdk);
-console.log('✅ All exports are working correctly'); 
